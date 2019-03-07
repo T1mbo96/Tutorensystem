d6802319567f0ca4d989c210b3d72332e18a8df8
Timo Michel <TimoM96@outlook.de>
2019-03-07 17:16:14 +0100

    Added different behavior for mobile access for blockly games

--- PythonTutorSystem/static/js/lesson_1/blockly_games_iframes.js
+++ PythonTutorSystem/static/js/lesson_1/blockly_games_iframes.js
@@ -3,3 +3,11 @@ function show_iframe() {
     document.getElementById('blockly_games_iframe').style.visibility = 'visible';
     document.getElementById('loading_icon').style.display = 'none';
 }
+
+window.onload(function () {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Mobile|Opera Mini/i.test(navigator.userAgent)) {
+        document.getElementById('loading_icon').style.display = 'none';
+        document.getElementById('blockly_games_iframe').style.display = 'none';
+        document.getElementById('mobile_blockly_games').style.display = 'block';
+    }
+});

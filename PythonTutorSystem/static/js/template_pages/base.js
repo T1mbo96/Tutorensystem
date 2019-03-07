1a81d9696c00f6e5946b35b4049755757938aa00
Timo Michel <TimoM96@outlook.de>
2019-03-07 22:29:03 +0100

    Added exceptions

--- PythonTutorSystem/static/js/template_pages/base.js
+++ PythonTutorSystem/static/js/template_pages/base.js
@@ -1,20 +1,3 @@
-// Open dropdown links in navbar when hovered
-function toggleDropdown(e) {
-    const _d = $(e.target).closest('.dropdown'),
-        _m = $('.dropdown-menu', _d);
-    setTimeout(function () {
-        const shouldOpen = e.type !== 'click' && _d.is(':hover');
-        _m.toggleClass('show', shouldOpen);
-        _d.toggleClass('show', shouldOpen);
-        $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
-        // Time till dropdown closes
-    }, e.type === 'mouseleave' ? 50 : 0);
-}
-
-$('body')
-    .on('mouseenter mouseleave', '.dropdown', toggleDropdown)
-    .on('click', '.dropdown-menu a', toggleDropdown);
-
 // Delay showing the animated content until everything loaded
 $(document).ready(function () {
     if (document.getElementById('load_animation_div')) {
